3b1e0569abf90dbd9d21e80b1a779ddbda74c7e3
bizoon <r.aureli@protonmail.com>
2021-11-02 09:27:16 +0000

    Completo

---
+++ database/migrations/2021_11_02_084654_create_trips_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTripsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trips', function (Blueprint $table) {
+            $table->id();
+            $table->string('destination_city');
+            $table->string('departure_city');
+            $table->string('id_customers');
+            $table->date('departure_date');
+            $table->date('return_date');
+            $table->integer('total_participants');
+            $table->float('cost_for_person');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trips');
+    }
+}
